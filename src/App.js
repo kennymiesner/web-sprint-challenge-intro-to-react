0dd4486a83e325f90ecafaa31f2c096cba0198a3
kennymiesner <kennymiesner@gmail.com>
2021-07-16 10:07:05 -0500

    Added effect hook and confirmed data flow

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react'; // Import state and effect hooks
 import axios from 'axios' // Import axios
 import Character from './components/Character' // Import character component
-import data from './mocks/handlers' // Import Star Wars data
+// import data from './mocks/handlers' // Import Star Wars data
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characterData, setCharacterData] = useState(data)
+  const [characterData, setCharacterData] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then(res => {
+      console.log(res.data)
+      setCharacterData(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
 
   return (
     <div className="App">
